83e1b748f2fe80594325d221dd0efcd6c1d4658a
hridoy <hridoytareqmahmud@gmail.com>
2021-01-06 14:09:45 +0600

    Add project files.

---
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+        private readonly ILogger<WeatherForecastController> _logger;
+        private IMongoCollection<Shipwreck> _shipwreckCollection;
+        public WeatherForecastController(IMongoClient clint)
+        {
+            var databse = clint.GetDatabase("sample_geospatial");
+            _shipwreckCollection = databse.GetCollection<Shipwreck>("shipwrecks");
+        }
+
+        [HttpGet]
+        public IEnumerable<Shipwreck> Get()
+        {  
+            return _shipwreckCollection.Find( s => s.FeatureType == "Wrecks - Visible").ToList() ;
+        }
+    }
+}
---
+++ WebApplication2/Shipwreck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace WebApplication2
+{
+    [BsonIgnoreExtraElements]
+    public class Shipwreck
+    {
+        [BsonId]
+        public ObjectId  Id { get; set; }
+        
+        [BsonElement("feature_type")]
+        public string FeatureType { get; set; }
+        [BsonElement("chart")]
+        public string Chart { get; set; }
+        [BsonElement("latdec")]
+        public double Latitude { get; set; }
+        [BsonElement("londec")]
+        public double Longitude { get; set; }
+
+    }
+}
