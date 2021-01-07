cb66fc438025603382eacc20a214c7ba1dc21246
hridoy <hridoytareqmahmud@gmail.com>
2021-01-07 15:42:00 +0600

    add bulkMongoOperation

--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -12,7 +12,8 @@ namespace WebApplication2.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-       
+        
+
         private readonly ILogger<WeatherForecastController> _logger;
         private IMongoCollection<Shipwreck> _shipwreckCollection;
         public WeatherForecastController(IMongoClient clint)
@@ -22,9 +23,16 @@ namespace WebApplication2.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Shipwreck> Get()
-        {  
-            return _shipwreckCollection.Find( s => s.FeatureType == "Wrecks - Visible").ToList() ;
+        public async Task<IEnumerable<Shipwreck>> Get()
+        {
+            var listWrites = new List<WriteModel<Shipwreck>>();
+            var filterDefinition = Builders<Shipwreck>.Filter.Eq(p => p.FeatureType, "Wrecks - Visible");
+            var updateDefinition = Builders<Shipwreck>.Update.Set(p => p.FeatureType, "Wrecks - Hridoy");
+            listWrites.Add(new UpdateOneModel<Shipwreck>(filterDefinition, updateDefinition));
+            await _shipwreckCollection.BulkWriteAsync(listWrites);
+           // _shipwreckCollection.BulkWrite(listWrites);
+            var tmp =  _shipwreckCollection.Find(s => s.FeatureType == "Wrecks - Hridoy").ToList();
+            return tmp;
         }
     }
 }
